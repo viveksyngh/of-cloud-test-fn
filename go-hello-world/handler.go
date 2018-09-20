ba0a0aba5f84a8634560fe1274fa5a7554156ddc
Vivek Kumar Singh <vivekkmr45@yahoo.in>
2018-09-20 17:31:07 +0530

    Update handler.go

--- go-hello-world/handler.go
+++ go-hello-world/handler.go
@@ -4,7 +4,7 @@ import (
 	"fmt"
 )
 
-// Handle a serverless function request 
+// Handle a serverless request 
 func Handle(req []byte) string {
 // 	return fmt.Sprintf("Hello, Go. You said: %s", string(req))
 }
