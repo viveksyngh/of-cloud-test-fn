a1d2ff9eac7a6049714902e5be8c1ae278edb1f5
Vivek Singh <vivekkmr45@yahoo.in>
2018-11-05 00:02:23 +0530

    Update nodejs function
    Signed-off-by: Vivek Singh <vivekkmr45@yahoo.in>

--- node-func/handler.js
+++ node-func/handler.js
@@ -1,5 +1,7 @@
 "use strict"
 
+
 module.exports = (context, callback) => {
-    callback(undefined, {status: "done", message: "Hello world from node funcion"});
+
+    callback(undefined, {status: "done", message: "Hello world from node funcion " + context});
 }
